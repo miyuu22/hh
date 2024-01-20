1b5b453e62dd6bcfaea40bb0f699c7c6095920f4
miyuu22 <139205386+miyuu22@users.noreply.github.com>
2024-01-20 12:16:50 +0900

    Add files via upload

---
+++ src/js/keyword.js
@@ -0,0 +1,79 @@
+
+function up() {
+    // idを取得
+        popup = document.getElementById("text");
+        // 詳細を表示
+        if (popup.style.display === "none" || popup.style.display === "" ) {
+            popup.style.display = "block";
+            popoup1.style.display = "block";
+        }else if(popup.style.display !== "none"){
+            popup.style.display = "none";
+        }
+      }
+
+function up1() {
+    // idを取得
+        popup = document.getElementById("text1");
+        // 詳細を表示
+        if (popup.style.display === "none" || popup.style.display === "" ) {
+            popup.style.display = "block";
+            popoup1.style.display = "block";
+        }else if(popup.style.display !== "none"){
+            popup.style.display = "none";
+        }
+        }
+function up2() {
+    // idを取得
+        popup = document.getElementById("text2");
+        // 詳細を表示
+        if (popup.style.display === "none" || popup.style.display === "" ) {
+            popup.style.display = "block";
+            popoup1.style.display = "block";
+        }else if(popup.style.display !== "none"){
+            popup.style.display = "none";
+        }
+        }
+function up3() {
+    // idを取得
+        popup = document.getElementById("text3");
+        // 詳細を表示
+        if (popup.style.display === "none" || popup.style.display === "" ) {
+            popup.style.display = "block";
+            popoup1.style.display = "block";
+        }else if(popup.style.display !== "none"){
+            popup.style.display = "none";
+        }
+        }
+function up4() {
+    // idを取得
+        popup = document.getElementById("text4");
+        // 詳細を表示
+        if (popup.style.display === "none" || popup.style.display === "" ) {
+            popup.style.display = "block";
+            popoup1.style.display = "block";
+        }else if(popup.style.display !== "none"){
+            popup.style.display = "none";
+        }
+        }
+function up5() {
+    // idを取得
+        popup = document.getElementById("text5");
+        // 詳細を表示
+        if (popup.style.display === "none" || popup.style.display === "" ) {
+            popup.style.display = "block";
+            popoup1.style.display = "block";
+        }else if(popup.style.display !== "none"){
+            popup.style.display = "none";
+        }
+        }
+function up6() {
+    // idを取得
+        popup = document.getElementById("text6");
+        // 詳細を表示
+        if (popup.style.display === "none" || popup.style.display === "" ) {
+            popup.style.display = "block";
+            popoup1.style.display = "block";
+        }else if(popup.style.display !== "none"){
+            popup.style.display = "none";
+        }
+        }
